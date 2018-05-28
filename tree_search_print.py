b4195378b56d33196fa69393fb120dc19e414370
helen98 <30391676+helen98@users.noreply.github.com>
2018-05-28 22:15:46 +0200

    Update tree_search_print.py

--- tree_search_print.py
+++ tree_search_print.py
@@ -60,3 +60,28 @@ class BinaryTree(object):
         if start.right:
             self.result = self.result + '-'
             self.preorder_print(start.right, start.right.value)
+    
+           
+
+
+
+# Set up tree
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+
+# Test search
+# Should be True
+print tree.search(4)
+
+# Should be False
+print tree.search(6)
+
+
+# Test print_tree
+# Should be 1-2-4-5-3
+print tree.print_tree()
+# tree.print_tree()
+
