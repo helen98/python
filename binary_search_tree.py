e1584ab5631db2ed8a547c408f9dc8e62e29aa70
helen98 <30391676+helen98@users.noreply.github.com>
2018-05-29 12:23:38 +0200

    Update binary_search_tree.py
    Insert and search functions in BST

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -7,38 +7,69 @@ class Node(object):
 class BST(object):
     def __init__(self, root):
         self.root = Node(root)
-
+        
+        
+        
+        
+        
     def insert(self, new_val):
-        node = self.root
-        self.helper_insert(node, new_val)
-    
-    def helper_insert(self, node, new_val):
-        if node == None:
-            node = Node(new_val)
-        if new_val < node.value:
-            return self.helper_insert(node.left, new_val)
-        elif new_val > node.value:
-            return self.helper_insert(node.right, new_val)
-        return
+        'Insearts new values in the tree according to the rule: smaller - to the left, gtreater - to the right'
+        root = self.root
+        self.insert_help(root, Node(new_val))
+
+    def insert_help(self, root, new_val):
+        'Recursive helper function'
+        if root is None:
+            # when hit the free spot, insert new value
+            root = new_val
+        else:
+            if root.value > new_val.value:
+                # look left
+                if root.left is None:
+                    # found free spot => insert new value
+                    root.left = new_val
+                else:
+                    # go down the tree to the left child
+                    self.insert_help(root.left, new_val)
+            else:
+                # look right
+                if root.right is None:
+                    # found free spot => insert new value
+                    root.right = new_val
+                else:
+                    # go down the tree to the right child
+                    self.insert_help(root.right, new_val)
+                    
+                    
+                    
+                    
             
     def search(self, find_val):
+        'Searches for the given value in the tree using helper function'
         start = self.root
         return self.helper_search(start, find_val)
         
     
     def helper_search(self, start, find_val):
+        'Recursive helper function'
         if find_val < start.value:
+            # look left
             if start.left == None:
                 return False
             return self.helper_search(start.left, find_val)
         elif find_val > start.value:
+            # look right
             if start.right == None:
                 return False
             return self.helper_search(start.right, find_val)
         else:
+            # find_val = start.value => found!!!!
             return True
-        
-    
+            
+            
+            
+            
+            
 # Set up tree
 tree = BST(4)
 
@@ -53,3 +84,8 @@ tree.insert(5)
 print tree.search(4)
 # Should be False
 print tree.search(6)
+
+
+
+
+    
